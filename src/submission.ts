f2be3e887e25a99f1b90704f6f0dd9df60e7c80b
Your Name <you@example.com>
2020-08-04 22:15:07 +1000

    Adds more attributions, and revamps the substitution algorithm.

--- src/submission.ts
+++ src/submission.ts
@@ -10,24 +10,34 @@ function shuffleFisherYates<T>(array: T[]) {
   return array;
 }
 
+function substitute(section: string) {
+  const split = section.split(' ');
+  const newSplitSection = [];
+  for (const word of split) {
+    let sub = word;
+    for (const entry of SUBSTITUION_MAP) {
+      sub = sub.replace(entry[0], getContent(entry[1]));
+    }
+    newSplitSection.push(sub);
+  }
+  return newSplitSection.join(' ');
+}
+
 export class Submission {
   constructor(private readonly sections: string[]) {
     this.sections = shuffleFisherYates(this.sections);
   }
 
   toString() {
-    const introduction = SUBMISSION_HEADER, conclusion = SUBMISSION_FOOTER;
-    const body = this.sections.join(' ');
-    const map = SUBSTITUION_MAP;
-    let submission = `
+    const introduction = substitute(SUBMISSION_HEADER);
+    const conclusion = substitute(SUBMISSION_FOOTER);
+    const body = [...this.sections.map(section => substitute(section))].join('\n\n');
+    const submission = `
 ${introduction}
 
 ${body}
 
 ${conclusion}`.trim();
-    for (const entry of map) {
-      submission = submission.replace(entry[0], getContent(entry[1]));
-    }
     return submission;
   }
 }
